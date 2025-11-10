8ce410282792e963d0197cd86aef44f0fe170f0b
Jiaming Shen <shenjm@stanford.edu>
2025-11-10 02:34:20 -0800

    change dataset

--- dataset.py
+++ dataset.py
@@ -22,15 +22,19 @@ class SP500(Dataset):
         self.label = torch.load(label_file)
         self.edge_index = torch.load(edge_index_file)
         self.edge_attr = torch.load(edge_attr_file)
+        print(f"Dataset loaded with features shape {self.feats.shape}, label shape {self.label.shape}, edge_index shape {self.edge_index.shape}, edge_attr shape {self.edge_attr.shape}")
     
     def len(self):
         return self.feats.shape[-1] - self.past_window - self.fut_window
     
     def get(self, idx: int):
-        return Data(x=self.feats[:, :, idx:idx+self.past_window], edge_index=self.edge_index, edge_attr=self.edge_attr, y=self.label[:, idx+self.past_window:idx+self.past_window+self.fut_window])
+        return Data(x=self.feats[:, 2:, idx:idx+self.past_window], edge_index=self.edge_index, edge_attr=self.edge_attr, y=self.feats[:, 2, idx+self.past_window:idx+self.past_window+self.fut_window])
     
 if __name__ == "__main__":
-    dataset = SP500()
+    dataset = SP500(feat_file='./data/processed/feature_small.pt',
+                label_file='./data/processed/daily_log_return_y_small.pt',
+                edge_attr_file='./data/processed/edge_attr_small.pt',
+                edge_index_file='./data/processed/edge_index_small.pt')
     
     print(f"length {len(dataset)}")
     
